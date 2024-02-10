077c55800b70e51ef342dda31abbe6142652148f
Getnet Shiferaw Mekonnen <107830977+Getnetsh@users.noreply.github.com>
2024-02-10 22:25:56 +0300

    update

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,14 +1,39 @@
 #!/usr/bin/python3
+""" test console
 """
-Unittest for the Console class
-Test files by using the following command line:
-python3 -m unittest tests/test_console.py
-"""
+
+
 import unittest
-import pep8
-from os import path, remove
+import sys
+from console import HBNBdata
+from unittest.mock import create_autospec
 
 
 class TestConsole(unittest.TestCase):
-    """define variables and methods"""
-    pass
+    """ class testing console
+    """
+    def setUp(self):
+        """ standard setUp """
+        self.mock_stdin = create_autospec(sys.stdin)
+        self.mock_stdout = create_autospec(sys.stdout)
+
+    def create(self, server=None):
+        """ create """
+        return HBNBdata(stdin=self.mock_stdin, stdout=self.mock_stdout)
+
+    def test_exit(self):
+        """ test exit command """
+        cli = self.create()
+        self.assertTrue(cli.onecmd("quit"))
+        self.assertTrue(cli.onecmd("EOF"))
+
+    def _last_write(self, nr=None):
+        """:return: last `n` output lines"""
+        if nr is None:
+            return self.mock_stdout.write.call_args[0][0]
+        return "".join(map(lambda c: c[0][0],
+                           self.mock_stdout.write.call_args_list[-nr:]))
+
+
+if __name__ == "__main__":
+    unittest.main()
